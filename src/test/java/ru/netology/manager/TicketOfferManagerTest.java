85ff46e00a0131eb052ee3185d0a83190edccbc3
Ekaterina Kolesnikova <1005885@mail.ru>
2021-07-10 20:29:09 +0300

    Update TicketOfferManagerTest

--- src/test/java/ru/netology/manager/TicketOfferManagerTest.java
+++ src/test/java/ru/netology/manager/TicketOfferManagerTest.java
@@ -12,10 +12,16 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 public class TicketOfferManagerTest {
     TicketOfferRepository repository = new TicketOfferRepository();
     TicketOfferManager manager = new TicketOfferManager(repository);
-    private TicketOffer first = new TicketOffer(1, 100_000, "KUF", "OGZ", 60);
-    private TicketOffer second = new TicketOffer(2, 800_000, "FRU", "EGO", 80);
-    private TicketOffer third = new TicketOffer(3, 200_000, "GOJ", "KUF", 100);
-    private TicketOffer fourth = new TicketOffer(4, 900_000, "GOJ", "KUF", 120);
+    private TicketOffer first = new TicketOffer(101, 100_000, "LED", "KUF", 60);
+    private TicketOffer second = new TicketOffer(202, 800_000, "FRU", "EGO", 80);
+    private TicketOffer third = new TicketOffer(303, 200_000, "GOJ", "KUF", 100);
+    private TicketOffer fourth = new TicketOffer(404, 900_000, "GOJ", "KUF", 120);
+    private TicketOffer fifth = new TicketOffer(505, 300_000, "KUF", "OGZ", 60);
+    private TicketOffer sixth = new TicketOffer(606, 400_000, "FRU", "EGO", 80);
+    private TicketOffer seventh = new TicketOffer(707, 700_000, "LED", "KUF", 140);
+    private TicketOffer eighth = new TicketOffer(808, 500_000, "LED", "KUF", 120);
+    private TicketOffer ninth = new TicketOffer(909, 600_000, "LED", "KUF", 160);
+    private TicketOffer tenth = new TicketOffer(1001, 1000_000, "GOJ", "KUF", 120);
 
     @BeforeEach
     public void setUp() {
@@ -23,35 +29,53 @@ public class TicketOfferManagerTest {
         manager.add(second);
         manager.add(third);
         manager.add(fourth);
+        manager.add(fifth);
+        manager.add(sixth);
+        manager.add(seventh);
+        manager.add(eighth);
+        manager.add(ninth);
+        manager.add(tenth);
     }
 
     // Сортировка предложений по цене;
     @Test
     public void shouldSortByCost() {
-        TicketOffer[] expected = new TicketOffer[]{first, third, second, fourth};
-        TicketOffer[] actual = new TicketOffer[]{first, second, third, fourth};
+        TicketOffer[] expected = new TicketOffer[]{first, third, fifth, sixth, eighth, ninth, seventh,
+                second, fourth, tenth};
+        TicketOffer[] actual = new TicketOffer[]{first, second, third, fourth, fifth, sixth, seventh, eighth,
+                ninth, tenth};
 
         Arrays.sort(actual);
 
         assertArrayEquals(actual, expected);
     }
 
-    // Найти одно предложение, для покупки билета, по аэропорту вылета и прилета;
+    // Найти два предложения, для покупки билета, по аэропорту вылета и прилета;
     @Test
-    public void shouldFindOneTicketOffer() {
+    public void shouldFindTwoTicketOffer() {
 
-        TicketOffer[] expected = new TicketOffer[]{second};
+        TicketOffer[] expected = new TicketOffer[]{sixth, second};
         TicketOffer[] actual = manager.searchBy("FRU", "EGO");
 
         assertArrayEquals(actual, expected);
     }
 
-    // Найти два предложения, для покупки билета, по аэропорту вылета и прилета;
+    // Найти три предложения, для покупки билета, по аэропорту вылета и прилета;
     @Test
-    public void shouldFindTwoTicketsOffer() {
+    public void shouldFindThreeTicketsOffer() {
 
-        TicketOffer[] expected = new TicketOffer[]{third, fourth};
+        TicketOffer[] expected = new TicketOffer[]{third, fourth, tenth};
         TicketOffer[] actual = manager.searchBy("GOJ", "KUF");
+
+        assertArrayEquals(actual, expected);
+    }
+
+    // Найти четыре предложения, для покупки билета, по аэропорту вылета и прилета;
+    @Test
+    public void shouldFindFourTicketsOffer() {
+
+        TicketOffer[] expected = new TicketOffer[]{first,  eighth, ninth, seventh};
+        TicketOffer[] actual = manager.searchBy("LED", "KUF");
 
         assertArrayEquals(actual, expected);
     }
@@ -70,9 +94,10 @@ public class TicketOfferManagerTest {
     @Test
     public void shouldRemoveByIdOneTicket() {
 
-        manager.removeById(2);
+        manager.removeById(202);
 
-        TicketOffer[] expected = new TicketOffer[]{first, third, fourth};
+        TicketOffer[] expected = new TicketOffer[]{first, third, fourth, fifth, sixth, seventh, eighth,
+                ninth, tenth};
         TicketOffer[] actual = repository.findAll();
 
         assertArrayEquals(actual, expected);
